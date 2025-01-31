2d4a21114033ad99d79bd6f1bd0991f07cb5313a
Ekta416 <ektapatidar416@gmail.com>
2025-01-31 23:59:52 +0530

    Revert "resolve"
    This reverts commit da4cdb064211182c6cc3e8788b7f3076a2178035.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const validator = require("validator");
